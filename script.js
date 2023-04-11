2035a28f3e1dfe8962a0ede5250a573f98dfa3c7
Chris Daniels <danielschris96@gmail.com>
2023-04-11 15:06:28 -0700

    manual merge of feature/bulma

--- script.js
+++ script.js
@@ -13,6 +13,46 @@ function removeDuplicateSuggestions(suggestions) {
   });
 }
 
+const inputField = document.getElementById('searched-input');
+const ingredientList = document.querySelector('.current-ingredients ul');
+
+inputField.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    const newIngredient = inputField.value.trim();
+    if (newIngredient !== '') {
+      const newListItem = document.createElement('li');
+      newListItem.textContent = newIngredient;
+      ingredientList.appendChild(newListItem);
+      inputField.value = '';
+    }
+  }
+});
+
+const filterCheckboxes = document.querySelectorAll('input[type="checkbox"]');
+
+filterCheckboxes.forEach(checkbox => {
+  checkbox.addEventListener('click', event => {
+    const filterText = event.target.parentNode.querySelector('span').textContent;
+    
+    const filterItem = document.createElement('li');
+    filterItem.textContent = filterText;
+    
+    const selectedFiltersList = document.getElementById('selected-filters-list');
+    
+    if (event.target.checked) {
+      if (!selectedFiltersList.querySelector(`li[data-filter="${filterText}"]`)) {
+        filterItem.setAttribute('data-filter', filterText);
+        selectedFiltersList.appendChild(filterItem);
+      }
+    } else {
+      const existingFilterItem = selectedFiltersList.querySelector(`li[data-filter="${filterText}"]`);
+      if (existingFilterItem) {
+        selectedFiltersList.removeChild(existingFilterItem);
+      }
+    }
+  });
+});
+
 async function searchFoodItemSuggestions(foodInput) {
   var spoonacularApiKey = "2e39a525784f4df6bc533d1a0e3e2403";
   var apiURLspoonacular = "https://api.spoonacular.com/food/ingredients/autocomplete?query=" + foodInput + "&number=10&apiKey=" + spoonacularApiKey;
@@ -50,7 +90,21 @@ searchInput.addEventListener('input', async () => {
           if (!selectedItems.includes(suggestion.name)) {
             selectedItems.push(suggestion.name);
             var selectedItem = document.createElement('div');
+            selectedItem.classList.add('selected-item');
             selectedItem.textContent = suggestion.name;
+          
+            var deleteButton = document.createElement('button');
+            deleteButton.textContent = 'x';
+            deleteButton.classList.add('delete-button');
+            deleteButton.addEventListener('click', function() {
+              var index = selectedItems.indexOf(suggestion.name);
+              if (index > -1) {
+                selectedItems.splice(index, 1);
+              }
+              selectedItem.remove();
+            });
+            
+            selectedItem.appendChild(deleteButton);
             selectedItemsList.appendChild(selectedItem);
           }
         });
