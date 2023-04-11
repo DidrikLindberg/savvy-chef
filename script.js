9b26556a9450bac8de3d4db97007d2b62b72ffc6
mtgibson8 <126990635+mtgibson8@users.noreply.github.com>
2023-04-11 15:31:10 -0500

    "replaced the JS, html, and CSS files with the most recent main branch code"

--- script.js
+++ script.js
@@ -13,111 +13,139 @@ function removeDuplicateSuggestions(suggestions) {
   });
 }
 
-async function searchFoodItemSuggestions(foodInput) {
-  var spoonacularApiKey = "2e39a525784f4df6bc533d1a0e3e2403";
-  var apiURLspoonacular = "https://api.spoonacular.com/food/ingredients/autocomplete?query=" + foodInput + "&number=10&apiKey=" + spoonacularApiKey;
+const inputField = document.getElementById('searched-input');
+const ingredientList = document.querySelector('.current-ingredients ul');
 
-  try {
-    var response = await fetch(apiURLspoonacular);
-    var suggestions = await response.json();
-    return suggestions;
-  } catch (error) {
-    console.error(error);
-    return [];
+inputField.addEventListener('keydown', function(event) {
+  if (event.key === 'Enter') {
+    const newIngredient = inputField.value.trim();
+    if (newIngredient !== '') {
+      const newListItem = document.createElement('li');
+      newListItem.textContent = newIngredient;
+      ingredientList.appendChild(newListItem);
+      inputField.value = '';
+    }
   }
-}
+});
 
-searchInput.addEventListener('input', async () => {
-  var searchTerm = searchInput.value.toLowerCase().trim();
-  suggestionBox.innerHTML = '';
 
-  if (searchTerm) {
-    var suggestions = await searchFoodItemSuggestions(searchTerm);
-    var uniqueSuggestions = removeDuplicateSuggestions(suggestions);
+const filterCheckboxes = document.querySelectorAll('.dropdown-menu input[type="checkbox"]');
 
-    suggestionBox.style.display = 'none';
-    suggestionBox.innerHTML = '';
+filterCheckboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', () => {
+    const filterLabel = checkbox.parentElement.querySelector('span').textContent;
 
-    if (uniqueSuggestions.length > 0) {
-      suggestionBox.style.display = 'block';
-      uniqueSuggestions.forEach(function(suggestion) {
-        var suggestionItem = document.createElement('div');
-        suggestionItem.textContent = suggestion.name;
-        suggestionItem.classList.add('suggestion-item');
-        suggestionItem.addEventListener('click', function() {
-          searchInput.value = suggestion.name;
-          suggestionBox.style.display = 'none';
-          if (!selectedItems.includes(suggestion.name)) {
-            selectedItems.push(suggestion.name);
-            var selectedItem = document.createElement('div');
-            selectedItem.textContent = suggestion.name;
-            selectedItemsList.appendChild(selectedItem);
-          }
-        });
-        suggestionBox.appendChild(suggestionItem);
-      });
+    if (checkbox.checked) {
+      const currentFiltersList = document.querySelector('.current-filters ul');
+      const filterItem = document.createElement('li');
+      filterItem.textContent = filterLabel;
+      currentFiltersList.appendChild(filterItem);
     } else {
-      suggestionBox.style.display = 'none';
+      const currentFiltersList = document.querySelector('.current-filters ul');
+      const filterItems = currentFiltersList.querySelectorAll('li');
+      filterItems.forEach(item => {
+        if (item.textContent === filterLabel) {
+          item.remove();
+        }
+      });
     }
-  } else {
-    suggestionBox.style.display = 'none';
-  }
+  });
 });
 
-let accessToken = '';
-let expirationTime = 0;
 
-async function getAccessToken() {
-  var clientId = 'recipepicker-10c9986c48c671640284d40976cd7e994572323350031644696';
-  var clientSecret = '7MqLtODQzHmZ6Qq_CGS5zE2JeSyGO84mXGRoKvoP';
-  var base64Credentials = btoa(`${clientId}:${clientSecret}`);
 
-  var response = await fetch('https://api.kroger.com/v1/connect/oauth2/token', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Basic ${base64Credentials}`,
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: 'grant_type=client_credentials&scope=product.compact'
-  });
+// var searchInput = document.getElementById('searched-input');
+// var suggestionBox = document.getElementById('suggested-item');
+// var selectedItemsList = document.getElementById('selected-items-list');
+// var selectedItems = [];
 
-  var data = await response.json();
-  accessToken = data.access_token;
-  expirationTime = Date.now() + (data.expires_in * 1000) - 60000;
-}
+// function removeDuplicateSuggestions(suggestions) {
+//   var seen = new Set();
+//   return suggestions.filter(function(suggestion) {
+//     var lowerCaseName = suggestion.name.toLowerCase();
+//     var isDuplicate = seen.has(lowerCaseName);
+//     seen.add(lowerCaseName);
+//     return !isDuplicate;
+//   });
+// }
 
-async function ensureAccessToken() {
-  if (Date.now() >= expirationTime) {
-    await getAccessToken();
-  }
-}
+// async function searchFoodItemSuggestions(foodInput) {
+//   var spoonacularApiKey = "2e39a525784f4df6bc533d1a0e3e2403";
+//   var apiURLspoonacular = "https://api.spoonacular.com/food/ingredients/autocomplete?query=" + foodInput + "&number=10&apiKey=" + spoonacularApiKey;
 
-async function getProductData(searchTerm, price) {
-  await ensureAccessToken();
+//   try {
+//     var response = await fetch(apiURLspoonacular);
+//     var suggestions = await response.json();
+//     return suggestions;
+//   } catch (error) {
+//     console.error(error);
+//     return [];
+//   }
+// }
 
-  var response = await fetch(`https://api.kroger.com/v1/products?filter.term=${searchTerm}&filter.limit=10`, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${accessToken}`,
-      'Accept': 'application/json'
-    }
-  });
+// searchInput.addEventListener('input', async () => {
+//   var searchTerm = searchInput.value.toLowerCase().trim();
+//   suggestionBox.innerHTML = '';
+
+//   if (searchTerm) {
+//     var suggestions = await searchFoodItemSuggestions(searchTerm);
+//     var uniqueSuggestions = removeDuplicateSuggestions(suggestions);
+
+//     suggestionBox.style.display = 'none';
+//     suggestionBox.innerHTML = '';
+
+//     if (uniqueSuggestions.length > 0) {
+//       suggestionBox.style.display = 'block';
+//       uniqueSuggestions.forEach(function(suggestion) {
+//         var suggestionItem = document.createElement('div');
+//         suggestionItem.textContent = suggestion.name;
+//         suggestionItem.classList.add('suggestion-item');
+//         suggestionItem.addEventListener('click', function() {
+//           searchInput.value = suggestion.name;
+//           suggestionBox.style.display = 'none';
+//           if (!selectedItems.includes(suggestion.name)) {
+//             selectedItems.push(suggestion.name);
+//             var selectedItem = document.createElement('div');
+//             selectedItem.textContent = suggestion.name;
+//             selectedItemsList.appendChild(selectedItem);
+//           }
+//         });
+//         suggestionBox.appendChild(suggestionItem);
+//       });
+//     } else {
+//       suggestionBox.style.display = 'none';
+//     }
+//   } else {
+//     suggestionBox.style.display = 'none';
+//   }
+// });
   
-  var data = await response.json();
-  return data.data.filter(product => product.price <= price);
-}
 
-document.querySelectorAll('.cost-dropdown-item').forEach(item => {
-  item.addEventListener('click', async event => {
-    event.preventDefault();
-    const price = parseFloat(item.dataset.price);
-    const searchTerm = searchInput.value.toLowerCase().trim();
-    if (searchTerm) {
-      const products = await getProductData(searchTerm, price);
-      console.log(products);
-    }
-  });
-});
+
+
+// yelp api
+// const apiKey = "sqTClu2QmI83wxtLBLe49xomsyJqkQHJnlb55lGNNajRfMoKMIHN6Nut0buqkf3wOAK54sVNKROwyDe8seq0_tKSo2SN_-8r0reYem3IMRj3zMkOm9vdE3Di1bk1ZHYx";
+// const url = `https://api.yelp.com/v3/businesses/search?term=mexican&location=San+Francisco`;
+// const headers = {
+//   Authorization: `Bearer ${apiKey}`,
+//   "Content-Type": "application/json"
+// };
+
+// async function fetchWithCORS(url, options) {
+//   const proxyURL = 'https://api.allorigins.win/raw?url=';
+//   const response = await fetch(proxyURL + encodeURIComponent(url), options);
+//   return response;
+// }
+
+// // Updated fetch call
+// fetchWithCORS(url, { headers })
+//   .then(response => response.json())
+//   .then(data => {
+//     // Handle the API response
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
 
 
 
