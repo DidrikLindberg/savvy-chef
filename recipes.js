65a20bac09e6ae368e3a2a766820f7de2be383cb
Chris Daniels <danielschris96@gmail.com>
2023-04-13 09:38:49 -0700

    changed styling of saved recipes

--- recipes.js
+++ recipes.js
@@ -1,22 +1,22 @@
 // Get the saved recipes from local storage
 var savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
-var resultsContainer = document.querySelector('#suggested-recipes');
+var savedRecipesTile = document.querySelector('#recipes-parent');
 // Loop through the saved recipes and create a container for each one
 savedRecipes.forEach(function(recipe) {
   // Create a container for the saved recipe
   var savedRecipeContainer = document.createElement('div');
   savedRecipeContainer.classList.add('saved-recipe-container');
 
-  // Create an image element for the recipe image
-  var recipeImage = document.createElement('img');
-  recipeImage.src = recipe.image;
-  recipeImage.alt = recipe.name;
-  savedRecipeContainer.appendChild(recipeImage);
-
   // Create a heading element for the recipe name
   var recipeName = document.createElement('h2');
   recipeName.textContent = recipe.name;
   savedRecipeContainer.appendChild(recipeName);
+
+// Create an image element for the recipe image
+  var recipeImage = document.createElement('img');
+  recipeImage.src = recipe.image;
+  recipeImage.alt = recipe.name;
+  savedRecipeContainer.appendChild(recipeImage);
 
   // Create a paragraph element for the recipe summary
   var recipeSummary = document.createElement('p');
@@ -24,5 +24,5 @@ savedRecipes.forEach(function(recipe) {
   savedRecipeContainer.appendChild(recipeSummary);
 
   // Add the saved recipe container to the results container
-  resultsContainer.appendChild(savedRecipeContainer);
+  savedRecipesTile.appendChild(savedRecipeContainer);
 });
