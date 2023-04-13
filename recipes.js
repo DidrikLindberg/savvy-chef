eacc49894da004a912349b971d1d37e97e78a79d
Chris Daniels <danielschris96@gmail.com>
2023-04-13 09:57:52 -0700

    added links to buttons that takes user to that part of page

--- recipes.js
+++ recipes.js
@@ -1,28 +1,28 @@
-// Get the saved recipes from local storage
+// get the saved recipes from local storage
 var savedRecipes = JSON.parse(localStorage.getItem('savedRecipes')) || [];
 var savedRecipesTile = document.querySelector('#recipes-parent');
-// Loop through the saved recipes and create a container for each one
+// loop through the saved recipes and create a container for each one
 savedRecipes.forEach(function(recipe) {
-  // Create a container for the saved recipe
+  // create a container for the saved recipe
   var savedRecipeContainer = document.createElement('div');
   savedRecipeContainer.classList.add('saved-recipe-container');
 
-  // Create a heading element for the recipe name
+  // create a heading element for the recipe name
   var recipeName = document.createElement('h2');
   recipeName.textContent = recipe.name;
   savedRecipeContainer.appendChild(recipeName);
 
-// Create an image element for the recipe image
+// create an image element for the recipe image
   var recipeImage = document.createElement('img');
   recipeImage.src = recipe.image;
   recipeImage.alt = recipe.name;
   savedRecipeContainer.appendChild(recipeImage);
 
-  // Create a paragraph element for the recipe summary
+  // create a paragraph element for the recipe summary
   var recipeSummary = document.createElement('p');
   recipeSummary.textContent = recipe.summary;
   savedRecipeContainer.appendChild(recipeSummary);
 
-  // Add the saved recipe container to the results container
+  // add the saved recipe to the results tile
   savedRecipesTile.appendChild(savedRecipeContainer);
 });
